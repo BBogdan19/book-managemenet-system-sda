72a86e2ed56bcd763f320dd81724c59e5a5fb829
Bogdan Bucea <bogdanbucea321@gmail.com>
2022-11-16 20:12:57 +0200

    BMS-17: Add generic repositories.

--- src/main/java/com/sda/bogdan/bookmanagement/repository/BaseRepository.java
+++ src/main/java/com/sda/bogdan/bookmanagement/repository/BaseRepository.java
@@ -1,2 +1,13 @@
-package com.sda.bogdan.bookmanagement.repository;public interface BaseRepository {
+package com.sda.bogdan.bookmanagement.repository;
+import java.util.Optional;
+
+public interface BaseRepository<T> {
+    Optional<T> findById(Integer id);
+
+    void create(T entity);
+
+    void update(T entity);
+
+    void delete(T entity);
+
 }
