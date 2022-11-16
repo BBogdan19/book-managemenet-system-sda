adc30a04de46f532fd3df9432e9f3b699c407cc9
Bogdan Bucea <bogdanbucea321@gmail.com>
2022-11-16 19:48:29 +0200

    BMS-16: Add hibernate model.

--- src/main/java/com/sda/bogdan/bookmanagement/Main.java
+++ src/main/java/com/sda/bogdan/bookmanagement/Main.java
@@ -1,7 +1,9 @@
 package com.sda.bogdan.bookmanagement;
 
+import com.sda.bogdan.bookmanagement.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
-
+        SessionManager.getSessionFactory();
     }
 }
--- src/main/java/com/sda/bogdan/bookmanagement/utils/SessionManager.java
+++ src/main/java/com/sda/bogdan/bookmanagement/utils/SessionManager.java
@@ -1,6 +1,9 @@
 package com.sda.bogdan.bookmanagement.utils;
 
 
+import com.sda.bogdan.bookmanagement.model.Author;
+import com.sda.bogdan.bookmanagement.model.Book;
+import com.sda.bogdan.bookmanagement.model.Review;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -22,6 +25,9 @@ public class SessionManager extends AbstractSessionManager {
     @Override
     protected void setAnnotatedClasses(Configuration configuration) {
         // Hibernate model will be added here
+        configuration.addAnnotatedClass(Author.class);
+        configuration.addAnnotatedClass(Book.class);
+        configuration.addAnnotatedClass(Review.class);
 
     }
 }
